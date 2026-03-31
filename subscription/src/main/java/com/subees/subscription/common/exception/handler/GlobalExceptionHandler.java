188f6b502888d552595c8cb7a0acd70ca053d5ac
gahyoung920-eng <gahyoung920@gmail.com>
2026-03-31 19:14:13 +0900

    ddl 수정 _ startdate enddate profileimage설정

--- subscription/src/main/java/com/subees/subscription/common/exception/handler/GlobalExceptionHandler.java
+++ subscription/src/main/java/com/subees/subscription/common/exception/handler/GlobalExceptionHandler.java
@@ -1,80 +1,33 @@
 package com.subees.subscription.common.exception.handler;
 
-import com.subees.subscription.common.exception.UniversityException;
-import com.subees.subscription.common.exception.dto.ApiErrorResponseDto;
-import lombok.extern.slf4j.Slf4j;
+import com.subees.subscription.common.exception.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-/*
-    스프링 예외 처리
-    1. @ControllerAdvice
-      - Controller 빈에서 발생하는 예외를 전역으로 처리할 수 있다.
-
-    2. @RestControllerAdvice
-      - RestController 빈에서 발생하는 예외를 전역으로 처리할 수 있다.
-
-    3. @ExceptionHandler
-      - 컨트롤러에서 발생하는 예외를 처리하는 메소드를 정의할 때 사용한다.
-      - 메소드에서 처리할 예외를 어노테이션의 value 속성으로 지정한다.
- */
-@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(UniversityException.class)
-    public ResponseEntity<ApiErrorResponseDto> handleException(UniversityException e) {
-        ApiErrorResponseDto apiErrorResponseDto = new ApiErrorResponseDto(
-                e.getHttpStatus().value(),
-                e.getStatus(),
-                e.getMessage()
-        );
 
-        log.error("UniversityException : {}", e.getMessage());
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequest(IllegalArgumentException e) {
+        ErrorResponse response = ErrorResponse.builder()
+                .errorcode(400)
+                .status("NOT ALLOWED")
+                .message(e.getMessage())
+                .build();
 
-        return new ResponseEntity<>(apiErrorResponseDto, e.getHttpStatus());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiErrorResponseDto> handleException(MethodArgumentNotValidException e) {
-        StringBuilder errors = new StringBuilder();
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<ErrorResponse> handleConflict(IllegalStateException e) {
+        ErrorResponse response = ErrorResponse.builder()
+                .errorcode(409)
+                .status("CONFLICT")
+                .message(e.getMessage())
+                .build();
 
-        log.error("MethodArgumentNotValidException : {}", e.getMessage());
-
-        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
-            errors
-                    .append(fieldError.getField())
-                    .append("(")
-                    .append(fieldError.getDefaultMessage())
-                    .append("), ");
-        }
-
-        errors.replace(errors.lastIndexOf(","),  errors.length(), "");
-
-        return new ResponseEntity<>(
-                new ApiErrorResponseDto(
-                        HttpStatus.BAD_REQUEST.value(),
-                        HttpStatus.BAD_REQUEST.name(),
-                        errors.toString()
-                ),
-                HttpStatus.BAD_REQUEST
-        );
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiErrorResponseDto> handleException(Exception e) {
-        ApiErrorResponseDto apiErrorResponseDto = new ApiErrorResponseDto(
-                HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                HttpStatus.INTERNAL_SERVER_ERROR.name(),
-                e.getMessage()
-        );
-
-        log.error("Global Exception : {}", e.getMessage());
-
-
-        return new ResponseEntity<>(apiErrorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
     }
 }
