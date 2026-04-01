37a6b9aa273a7e9388c771bf847d5dc57915109a
ZonezIpex <aksvmfkd3@naver.com>
2026-04-01 11:34:31 +0900

    mybatis change finish

--- src/main/java/com/subees/backend/user/entity/User.java
+++ src/main/java/com/subees/backend/user/entity/User.java
@@ -1,6 +1,5 @@
 package com.subees.backend.user.entity;
 
-import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,40 +7,18 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Entity
-@Table(name = "user")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
     private Long userId;
-
-    @Column(nullable = false, length = 255, unique = true)
     private String email;
-
-    @Column(nullable = false, length = 255)
     private String password;
-
-    @Column(nullable = false, length = 255, unique = true)
     private String nickname;
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "user_state", nullable = false)
     private UserState userState;
-
-    @Column(name = "email_verified", nullable = false, length = 1)
     private String emailVerified;
-
-    @Column(name = "email_verified_at")
     private LocalDateTime emailVerifiedAt;
-
-    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
-
-    @Column(name = "profile_image_url", length = 500)
     private String profileImageUrl;
 
     @Builder
